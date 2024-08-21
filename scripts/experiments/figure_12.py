2473d58d59e3d7aacd7fbde365cb7ec5e9c99038
Jiazhen Lin <linjz20@mails.tsinghua.edu.cn>
2024-08-21 23:17:18 +0000

    add figure12 (a)

--- scripts/experiments/figure_12.py
+++ scripts/experiments/figure_12.py
@@ -3,27 +3,58 @@ from commons import run_item
 url = "http://10.0.2.144:8080/figure/12"
 
 
-def round(args):
+def round_a(args, lc_bw_list):
+    res = []
+    for i in lc_bw_list:
+        args["lc_bw"] = i
+        args["op"] = "a"
+        res_item = run_item(url, args)
+        res_item["lc_bw"] = i
+        res.append(res_item)
+    return res
+
+
+def round_b(args):
     res = []
     for i in [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]:
         args["th"] = i
+        args["op"] = "b"
         res_item = run_item(url, args)
         res_item["th"] = i
         res.append(res_item)
     return res
 
 
+print("(a)")
+print("- Caladan")
+args = {"type": "caladan", "op": "a"}
+lc_bw_list = [0.3, 0.9, 2.1, 2.7, 3, 4.5, 6, 9]
+caladan_res = round_a(args, lc_bw_list)
+print("| lc_bw | bw | p999 | \n|---|---|---|")
+for line in caladan_res:
+    print(f"|{line['lc_bw']}\t|{line['bw']}\t|{line['p999']}\t|")
+
+print("- Vessel")
+args = {"type": "vessel", "op": "a"}
+lc_bw_list = [0.3, 0.9, 2.1, 2.7, 3, 4.5, 6, 6.6, 9]
+vessel_res = round_a(args, lc_bw_list)
+print("| lc_bw | bw | p999 | \n|---|---|---|")
+for line in vessel_res:
+    print(f"|{line['lc_bw']}\t|{line['bw']}\t|{line['p999']}\t|")
+
+
+print("(b)")
 print("- MBA")
 args = {"type": "mba"}
-mba_res = round(args)
+mba_res = round_b(args)
 
 print("- Linux")
 args = {"type": "linux"}
-linux_res = round(args)
+linux_res = round_b(args)
 
 print("- Vessel")
 args = {"type": "vessel"}
-vessel_res = round(args)
+vessel_res = round_b(args)
 
 print("### Bandwidth")
 print("| th | mba | linux | vessel | \n|---|---|---|---|")
